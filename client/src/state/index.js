865da349d163dc0e28474c49f700bff3445dfc1d
Dhyey Mistri <dhyeymistri27@gmail.com>
2023-01-11 19:01:34 +0530

    Homepage widget

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -18,6 +18,10 @@ export const authSlice = createSlice({
             state.user = action.payload.user;
             state.token = action.payload.token;
         },
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        },
         setFriends: (state, action) => {
             if (state.user) {
                 state.user.friends = action.payload.friends;
